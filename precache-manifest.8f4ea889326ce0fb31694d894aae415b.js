d7a1424a10d75bfc4006210616783ece0cb3190f
nt031198 <43611696+nt031198@users.noreply.github.com>
2022-11-27 12:41:50 +0530

    Updates

---
+++ precache-manifest.8f4ea889326ce0fb31694d894aae415b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "345e22a1653e234510b6",
+    "url": "/Album-list/static/css/main.59809d58.chunk.css"
+  },
+  {
+    "revision": "345e22a1653e234510b6",
+    "url": "/Album-list/static/js/main.345e22a1.chunk.js"
+  },
+  {
+    "revision": "239dc32d5f21e50e6aa6",
+    "url": "/Album-list/static/js/1.239dc32d.chunk.js"
+  },
+  {
+    "revision": "546bf2a9b1574032388a",
+    "url": "/Album-list/static/js/runtime~main.546bf2a9.js"
+  },
+  {
+    "revision": "1018bcbc94fe96d5b1ce6342e73ece1c",
+    "url": "/Album-list/index.html"
+  }
+];
